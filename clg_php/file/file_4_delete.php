2b22c309c1b205b1b061d8573e7e3cd588de36f5
SK SINGH <singhsushil5277@gmail.com>
2022-01-23 22:12:20 +0545

    [UPDATE] Delete file

--- clg_php/file/file_4_delete.php
+++ clg_php/file/file_4_delete.php
@@ -1,7 +1,12 @@
 <?php
+$filename = 'test.txt';
 
-
-
-
+if(file_exists($filename))
+{
+    unlink($filename);
+    echo $filename. ' file deleted successfully.';
+}else{
+    echo "File not found.";
+}
 
 ?>
