288bd5a133ff5036507ebada5500a84ad2057088
SK SINGH <singhsushil5277@gmail.com>
2022-01-23 22:02:59 +0545

    adde example of "include"

---
+++ clg_php/include_demo/demo.php
@@ -0,0 +1,30 @@
+<?php include 'header.php';  ?>
+
+<table>
+    <tr>
+        <th>One</th>
+        <th>two</th>
+        <th>Three</th>
+        <th>Four</th>
+        <th>Five</th>
+    </tr>
+
+    <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+    </tr>
+
+    <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+    </tr>
+
+</table>
+
+<?php include 'footer.php';  ?>
