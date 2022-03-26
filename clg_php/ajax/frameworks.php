4b7a6c4b4a7416f4b9deaf5fc8b58bedf67a14c8
SK SINGH <singhsushil5277@gmail.com>
2022-03-26 18:10:49 +0545

    Added example of AJAX

---
+++ clg_php/ajax/frameworks.php
@@ -0,0 +1,33 @@
+<?php
+
+$frameworks = array("CodeIgniter","Zend Framework","Cake PHP","Kohana") ;
+
+$name = $_GET["name"];
+
+if (strlen($name) > 0) {
+
+    $match = "";
+
+    for ($i = 0; $i < count($frameworks); $i++) {
+
+        if (strtolower($name) == strtolower(substr($frameworks[$i], 0, strlen($name)))) {
+
+            if ($match == "") {
+
+                $match = $frameworks[$i];
+
+            } else {
+
+                $match = $match . " , " . $frameworks[$i];
+
+            }
+
+        }
+
+    }
+
+}
+
+echo ($match == "") ? 'no match found' : $match;
+
+?>
---
+++ clg_php/ajax/index.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title>PHP MVC Frameworks - Search Engine</title>
+        <script>
+            function showName(str) {            
+                    if (str.length == 0) {
+                        document.getElementById("txtName").innerHTML = ""; //clear previous results
+                        return;
+                    }                    
+
+                    var xmlhttp = new XMLHttpRequest();
+                    //The XMLHttpRequest method open() initializes a newly-created request, or re-initializes an existing one.
+                    xmlhttp.open("GET", "frameworks.php?name=" + str);
+
+                    xmlhttp.onreadystatechange = function () {
+                        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                            if(xmlhttp.status == 200)
+                            {
+                                document.getElementById("txtName").innerHTML = xmlhttp.responseText;
+                            }
+                        }
+                    };
+                    //The XMLHttpRequest method send() sends the request to the server.
+                    xmlhttp.send();
+                }
+        </script>
+    </head>
+
+    <body>
+        <h2>PHP MVC Frameworks - Search Engine</h2>
+
+        <p><b>Type the first letter of the PHP MVC Framework</b></p>
+
+        <form>
+            <p><input type="text" size="40" id="txtHint"  onkeyup="showName(this.value)"></p>
+        </form>
+
+        <p>Matches: <span id="txtName"></span></p>
+
+    </body>
+</html>
